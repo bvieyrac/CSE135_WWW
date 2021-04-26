8cb33928158bce94a0664399dbd281b8253a8b72
kunkun9811 <timochen77@gmail.com>
2021-04-26 14:10:26 -0700

    rewrote python hello world

--- cse135hw1.site/public_html/cgi-bin/python-hello-html-world.py
+++ cse135hw1.site/public_html/cgi-bin/python-hello-html-world.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python
 
-#Import modules for CGI handling
+## Import modules for CGI handling
 import cgi, cgitb
 
+##  Headers
+print "Cache-Control: no-cache\n"
 print "Content-type:text/html\n\n"
+
+##  Head
 print "<html>"
 print "<head>"
 print "<title>Hello World!! by Timo Chen</title>"
 print "</head>"
+
+##  Body
 print "<body>"
-print "<h1>HELLO WORLD! by Timo Chen</h1>"
+
+print "<h1>Timo & Bradley were here - Hello, Perl!</h1>"
+print "<p>This page was generated with the Perl programming langauge</p>
+
+$date = localtime()
+print "<p>Current Time: $date</p>"
+
+# IP address
+$address = $ENV{REMOTE_ADDR}
+print "<p>Your IP Address: $address</p>"
+
 print "</body>"
 print "</html>"
