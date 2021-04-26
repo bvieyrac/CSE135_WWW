fe6b17d5110bca457ccaaaf56c52e37410034e42
kunkun9811 <timochen77@gmail.com>
2021-04-26 13:18:06 -0700

    Modified Hello World Python

--- cse135hw1.site/public_html/cgi-bin/python-hello-html-world.py
+++ cse135hw1.site/public_html/cgi-bin/python-hello-html-world.py
@@ -4,11 +4,11 @@
 import cgi, cgitb 
 
 # Create instance of FieldStorage 
-form = cgi.FieldStorage() 
+# form = cgi.FieldStorage() 
 
 # Get data from fields
-first_name = form.getvalue('first_name')
-last_name  = form.getvalue('last_name')
+# first_name = form.getvalue('first_name')
+# last_name  = form.getvalue('last_name')
 
 print("Content-type:text/html\r\n\r\n")
 print("<html>")
