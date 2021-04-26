628a34f6a090057a296a4ea3f0b32798355c5242
bvieyrac <brad.vieyracelaya@gmail.com>
2021-04-26 20:39:22 +0000

    hi

--- cse135hw1.site/public_html/cgi-bin/php-hello-html-world.php
+++ cse135hw1.site/public_html/cgi-bin/php-hello-html-world.php
@@ -1,1 +1,23 @@
-#!/usr/bin/cgi-php
+#!/usr/bin/php
+<?php
+print "Cache-Control: no-cache\n";
+print "Content-type: text/html\n\n";
+print "<html>";
+print "<head>";
+print "<title>Hello, PHP!</title>";
+print "</head>";
+print "<body>";
+
+print "<h1>Timo & Bradley were here - Hello, PHP!</h1>";
+print "<p>This page was generated with PHP</p>";
+
+//$date = localtime();
+//print "<p>Current Time: $date</p>";
+
+# IP Address is an environment variable when using CGI
+//$address = $ENV{REMOTE_ADDR};
+//print "<p>Your IP Address: $address</p>";
+
+print "</body>";
+print "</html>";
+?>
